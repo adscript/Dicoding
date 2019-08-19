b37a29f957b7b99bd9cf5dcaafbef5fb7e5d5362
adscript <adnanocode@gmail.com>
2019-08-19 11:35:53 +0700

    Project 2 Learn Intent

---
+++ 02. Beginner Android App/FirstIntentApp/app/src/main/java/com/adnano/firstintentapp/MoveActivity.java
@@ -0,0 +1,16 @@
+package com.adnano.firstintentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+
+
+    }
+
+}
